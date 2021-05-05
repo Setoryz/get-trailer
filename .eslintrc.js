372b3fb0aaa94ae1d182ab4d3778b997aa9028da
Odukoya Jesuseyitan <setoryz@gmail.com>
2021-05-05 19:58:02 +0100

    🎉 prettier, eslint, husky and lint-staged setup

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  parser: '@typescript-eslint/parser', // Specifies the ESLint parser
+  parserOptions: {
+    ecmaVersion: 2020, //Allows for parsing of modern ECMA Script features
+    sourceType: 'module', // Allows for the use of imports
+  },
+  plugins: ['@typescript-eslint'],
+  extends: [
+    'plugin:@typescript-eslint/recommended', // Uses the recommended rules from the @typescript-eslint/eslint-plugin
+    'eslint-config-prettier',
+    //// 'prettier/@typescript-eslint', // Uses eslint-config-prettier to disable ESLint rules from @typescript-eslint/eslint-plugin that would conflict with prettier
+    'plugin:prettier/recommended', // Enables eslint-plugin-prettier and eslint-config-prettier. this will display prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
+  ],
+  rules: {},
+};
