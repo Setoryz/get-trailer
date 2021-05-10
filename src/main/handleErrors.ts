4db2d0e00d85d19453938d625ecf01561703b3da
Odukoya Jesuseyitan <setoryz@gmail.com>
2021-05-10 15:30:08 +0100

    🔇 🚚 moved src files to main - Error object logging removed in handleErrors

--- src/main/handleErrors.ts
+++ src/main/handleErrors.ts
@@ -3,7 +3,6 @@
  * @param error Error to be handled
  */
 const handleErrors = (error: Error): void => {
-  console.warn(error);
   const message = error && error.message ? error.message : error;
   console.warn(`movie-trailer: ${message}`);
 };
