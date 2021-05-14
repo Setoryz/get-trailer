23270b9fb2294f43bd694d4abd578604e14b4fa0
Odukoya Jesuseyitan <setoryz@gmail.com>
2021-05-14 20:32:05 +0100

    default export added to index

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,3 @@
-import getTrailer from './getTrailer';
+import getTrailer from './main/getTrailer';
 
-const main = async () => {
-  console.log('starting...');
-  console.log('Birds of Prey');
-  console.log(await getTrailer('Birds of Prey', { year: 2010 }));
-
-  console.log();
-  console.log('Game of Thrones');
-  console.log(await getTrailer('Game of Thrones', { category: 'TV', multi: true, videoId: true }));
-  console.log();
-
-  console.log('93484');
-  console.log(await getTrailer(null, { category: 'TV', multi: true, tmdbId: '93484' }));
-  console.log();
-  // return getTrailer;
-};
-
-main();
+export default getTrailer;
